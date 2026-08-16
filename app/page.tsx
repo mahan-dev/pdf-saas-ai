c27e0b85f5b048d249f6b64590d98101249a7c5e
mahan.dev <mahansoodbakhsh@yahoo.com>
2026-08-16 22:44:19 +0330

    Home.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-
-
 import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
@@ -7,52 +5,43 @@ import Link from "next/link";
 
 import { LogIn } from "lucide-react";
 
-const page =async ()  => {
-  const {userId} = await auth()
+const page = async () => {
+  const { userId } = await auth();
 
   const isAuth = !!userId;
   return (
-    <div className="bg-linear-to-r from-indigo-300 to-purple-400 min-h-screen flex items-center justify-center">
+    <div className=" min-h-screen flex items-center justify-center">
       <div className="absolute top-1/2  left-1/2 -translate-y-1/2 -translate-x-1/2 text-center">
-      <div className="flex flex-col justify-center items-center">
-      <h1 className="text-5xl font-bold ">Chat with any PDF</h1>
-      <UserButton afterSwitchSessionUrl="/" />
-        
+        <div className="flex flex-col justify-center items-center">
+          <div className="flex gap-1.5 items-center">
+            <h1 className="text-5xl font-bold ">Chat with any PDF</h1>
+            <UserButton afterSwitchSessionUrl="/" />
+          </div>
 
+          <div className="flex flex-col gap-1 items-center">
+            {isAuth && <Button className={'mt-1.5'}>go to Chats</Button>}
+            <p className="text-slate-600 max-w-xl">
+              join millions of students, researchers and professionals to
+              instantly answer questions and understand research with ai
+            </p>
+          </div>
 
-      <div className="flex">
-        {
-          isAuth && (
-            <Button>go to Chats</Button> 
-          )
-        }
-        <p className="text-slate-600 max-w-xl">
-          join millions of students, researchers and professionals to instantly answer questions and understand research with ai
-        </p>
-        
+          <div className="w-full mt-2">
+            {isAuth ? (
+              <div>fileUpload</div>
+            ) : (
+              <Link href={"/sign-in"}>
+                <Button className="cursor-pointer">
+                  Login to get start
+                  <LogIn />
+                </Button>
+              </Link>
+            )}
+          </div>
         </div>
-
-        <div className="w-full mt-2">
-          {
-            isAuth ? <div>fileUpload</div> : 
-            <Link href={"/sign-in"}>
-            <Button className="cursor-pointer">Login to get start
-              <LogIn  />
-
-            </Button>
-            </Link>
-          }
-          
-        </div>
-
       </div>
-
-      </div>
-
-
-      
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import { SignUp } from "@clerk/nextjs";
+import React from "react";
 
 const page = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <SignUp />
+    </div>
+  );
 };
 
 export default page;
