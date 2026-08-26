98a4c05afd2b72d631d1cb6e38e81936d87eb5c3
mahan.dev <mahansoodbakhsh@yahoo.com>
2026-08-26 23:28:53 +0330

    chatSidebar

--- core/components/templates/ChatSidebar.tsx
+++ core/components/templates/ChatSidebar.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Button } from "../ui/button";
 import { PlusCircle } from "lucide-react";
 import { DrizzleChat } from "@/core/lib/db/schema";
+import { cn } from "@/core/lib/utils";
 
 interface ChatProps {
   chats: DrizzleChat[];
@@ -14,16 +15,29 @@ const ChatSidebar = ({ chats, chatId }: ChatProps) => {
     <section className="flex flex-1  max-w-xs">
       <div className="w-full p-3 h-screen bg-gray-900 overflow-auto">
         <Link className="w-full inline-block mb-3" href="/">
-          <Button className="w-full border border-dashed  border-white">
+          <Button
+            className={cn(
+              "w-full py-5 border border-dashed  border-white  cursor-pointer",
+            )}
+          >
             New Chat
             <PlusCircle className="w-4 h-4" />
           </Button>
         </Link>
 
-        <div className="flex flex-col">
+        <div className="flex flex-col gap-1">
           {chats?.map((item) => (
-            <Link href={"/"} key={item.id}>
-              <p className="text-red-500 break-all truncate ">{item.pdfName}</p>
+            <Link href={`/chat/${chatId}`} key={item.id}>
+              <p
+                className={`
+              text-white break-all truncate
+              ${item.id === chatId ? "hover:bg-none" : "transition-all duration-150 rounded-md p-2 hover:bg-zinc-600 "}
+            
+              ${item.id === chatId ? "bg-blue-700 rounded-md p-2 " : ""}
+                `}
+              >
+                {item.pdfName}
+              </p>
             </Link>
           ))}
         </div>
