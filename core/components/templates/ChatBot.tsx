0913f29f9579b9d239684c7090d90838e6791dd0
mahan.dev <mahansoodbakhsh@yahoo.com>
2026-09-05 22:03:35 +0330

    chatBot changes

--- core/components/templates/ChatBot.tsx
+++ core/components/templates/ChatBot.tsx
@@ -97,9 +97,9 @@ const ChatBot = ({ chatId }: ChatProps) => {
   }, [isOpen, setIsOpen]);
 
   return (
-    <section className="flex-3 max-w-sm max-lg:flex-0">
+    <section className="flex-3 max-w-sm max-lg:flex-0 overflow-y-auto">
       <div
-        className="flex bg-white absolute top-2 right-2 p-2 border rounded-md shadow-md   lg:hidden"
+        className="flex bg-white absolute top-2 right-2 p-2 border rounded-md shadow-md lg:hidden cursor-pointer"
         onClick={() => setIsOpen((prev) => !prev)}
         ref={botIconRef}
       >
@@ -121,13 +121,13 @@ const ChatBot = ({ chatId }: ChatProps) => {
         id="message_container"
         ref={messageContainerRef}
       >
-        <X
-          className="w-8 h-8 text-black-300 p-1 absolute  top-3  right-6 z-20   border rounded-md cursor-pointer lg:hidden "
-          onClick={() => setIsOpen((prev) => !prev)}
-        />
         <section className="w-full  flex-col p-1 min-h-screen  ">
-          <h3 className="text-2xl p-2 rounded-md font-bold sticky top-0 inset-x-0 backdrop-blur-2xl">
+          <h3 className="flex  justify-between text-2xl p-2 rounded-md font-bold sticky top-0 inset-x-0 backdrop-blur-2xl">
             Chat
+            <X
+              className="w-8 h-8 text-black-300 bg-white p-1 sticky  top-3  right-6 z-20   border rounded-md cursor-pointer lg:hidden "
+              onClick={() => setIsOpen((prev) => !prev)}
+            />
           </h3>
 
           <MessageList messages={messages} isLoading={isLoading} />
